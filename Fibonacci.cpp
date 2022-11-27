69b5490263d667dbc7219e5703b16d0bee667196
Aditya Srivastava <adi.official.2990@gmail.com>
2022-11-27 21:02:19 +0530

    Fibonacci Series with best Runtime

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -6,12 +6,21 @@ using namespace std;
 class Solution{ 
 public:
     int fib(int n) {
-       if(n == 1){
+
+        int prv1 = 0, prv2 = 1 , ans;
+       if(n == 1 || n == 2){
         return 1;
        }else if(n ==0){
         return 0;
        }else{
-        return fib(n-2)+ fib(n-1);
+        for (int i = 1; i < n; i++)
+        {
+            ans = prv1 + prv2;
+            prv1 = prv2;
+            prv2 = ans;
+        }
+        return ans;
+        
        }
 
     }
